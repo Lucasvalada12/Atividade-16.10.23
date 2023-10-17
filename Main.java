e351e12967724e8c99bede1dac3f04eab7cc37c5
Lucasvalada12 <122353470+Lucasvalada12@users.noreply.github.com>
2023-10-17 21:54:20 +0000

    primeiro commit

--- Main.java
+++ Main.java
@@ -2,6 +2,40 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
  Scanner reed = new Scanner(System.in);
+    int quadrado;
+
+    System.out.print("Digite um numero: ");
+    quadrado = reed.nextInt();
     
+    
+
+    for(int i = 0; i <= quadrado; i++){
+      for(int j = 0; j <= (quadrado - i); j++){
+        System.out.print("* ");
+      }
+      System.out.print("\n");
   }
+    System.out.print("\n");
+    
+    for(int i = 0; i <= quadrado; i++){
+        for(int j = 0; j <=  i; j++){
+          System.out.print("* ");
+        }
+        System.out.print("\n");
+    }
+    System.out.print("\n");
+    
+    for(int i = 0; i <= quadrado; i++){
+        for(int j = 0; j <=quadrado; j++){
+          System.out.print("* ");
+        }
+        System.out.print("\n");
+    }
+    for(int i = 0; i <= quadrado; i++){
+        for(int j = 0; j >= (quadrado + i); j++){
+          System.out.print("* ");
+        }
+        System.out.print("\n");
+    }
+}
 }
